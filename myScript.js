b1a19f27617665af75a8e2539a1ba51d3a1462d9
MoistButteryBiscuits <Mikeleblanc8489@gmail.com>
2021-12-02 19:45:10 -0500

    Add function to select rock, paper or scissors for computer

--- myScript.js
+++ myScript.js
@@ -1,1 +1,16 @@
-console.log("hello world")
+function computerPlay () {
+    let computerOption = [
+        "Rock",
+        "Paper",
+        "Scissors"
+    ];
+
+    //randomly selects a computer pick from computerOption array
+    let randomSelection = computerOption[Math.floor(Math.random() * computerOption.length)]
+    return randomSelection
+}
+
+//tests output
+//console.log(computerPlay());
+
+computerPlay();
