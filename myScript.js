c7e46928ae6850e731fe5f0c313bc3faeb115cab
MoistButteryBiscuits <Mikeleblanc8489@gmail.com>
2021-12-04 11:25:17 -0500

    Add psuedo for playRound function

--- myScript.js
+++ myScript.js
@@ -10,8 +10,21 @@ function computerPlay () {
     return computerSelection
 }
 
+function playRound (playerSelection, computerSelection) {
+    /*Pseudo
+    -Paper beats rock
+    -Rock beats scissors
+    -Scissor beats paper
+    
+    -send playerSelection into a variable that converts string into lowercase
+    -compare between playerSelection and computerSelection
+    -declare a winner using playerSelection and computerSelection variables
+    */
+}
+
 
 //tests output
 //console.log(computerPlay());
 
-computerPlay();
+const computerSelection = computerPlay();
+const playerSelection = "rock"
