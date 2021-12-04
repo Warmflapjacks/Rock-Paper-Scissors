356c33a2324fbe239226ff9bced07c6c83fc42b4
MoistButteryBiscuits <Mikeleblanc8489@gmail.com>
2021-12-04 12:55:04 -0500

    Add function game() to play 5 rounds per game

--- myScript.js
+++ myScript.js
@@ -10,51 +10,57 @@ function computerPlay () {
     return computerSelection
 }
 
-function playRound (playerSelection, computerSelection) {
-    //sends playerselection to lowercase
-    let playerPick = playerSelection.toLowerCase();
+function game() {
+    for (let i = 1; i <= 5; i++) {
+        function playRound (playerSelection, computerSelection) {
+            //sends playerselection to lowercase
+            let playerPick = playerSelection.toLowerCase();
 
-    //if selections are a tie
-    if (playerPick === computerSelection) {
-        console.log ("Its a tie!");
-        // return ("Its a tie!");
-    }
-    //used when not a tie
-    else{
-        if (playerPick === "paper") {
-            if (computerSelection === "rock") {
-                console.log ("You win! " + playerSelection + " beats " + computerSelection);
-                // return ("You win! " + playerSelection + " beats " + computerSelection);
+            //if selections are a tie
+            if (playerPick === computerSelection) {
+                console.log ("Its a tie!");
+                // return ("Its a tie!");
             }
-            else {
-                console.log("You lose! Scissors beats " + playerSelection);
-                // return ("You lose! Scissors beats " + playerSelection);
+            //used when not a tie
+            else{
+                if (playerPick === "paper") {
+                    if (computerSelection === "rock") {
+                        console.log ("You win! " + playerSelection + " beats " + computerSelection);
+                        // return ("You win! " + playerSelection + " beats " + computerSelection);
+                    }
+                    else {
+                        console.log("You lose! Scissors beats " + playerSelection);
+                        // return ("You lose! Scissors beats " + playerSelection);
+                    }
+                }
+                if (playerPick === "rock") {
+                    if (computerSelection === "scissors") {
+                        console.log("You win! " + playerSelection + " beats " + computerSelection);
+                        // return ("You win! " + playerSelection + " beats " + computerSelection);
+                    }
+                    else {
+                        console.log("You lose! Paper beats " + playerSelection);
+                        // return ("You lose! Paper beats " + playerSelection);
+                    }
+                }
+                if (playerPick === "scissors") {
+                    if (computerSelection === "paper") {
+                        console.log("You win! " + playerSelection + " beats " + computerSelection);
+                        // return ("You win! " + playerSelection + " beats " + computerSelection);
+                    }
+                    else {
+                        console.log("You lose! Rock beats " + playerSelection);
+                        // return ("You lose! Rock beats " + playerSelection);
+                    }
+                }
             }
         }
-        if (playerPick === "rock") {
-            if (computerSelection === "scissors") {
-                console.log("You win! " + playerSelection + " beats " + computerSelection);
-                // return ("You win! " + playerSelection + " beats " + computerSelection);
-            }
-            else {
-                console.log("You lose! Paper beats " + playerSelection);
-                // return ("You lose! Paper beats " + playerSelection);
-            }
-        }
-        if (playerPick === "scissors") {
-            if (computerSelection === "paper") {
-                console.log("You win! " + playerSelection + " beats " + computerSelection);
-                // return ("You win! " + playerSelection + " beats " + computerSelection);
-            }
-            else {
-                console.log("You lose! Rock beats " + playerSelection);
-                // return ("You lose! Rock beats " + playerSelection);
-            }
-        }
+        const computerSelection = computerPlay();
+        console.log ("Computer selection: " + computerSelection);  //Remove
+        const playerSelection = "Scissors";
+
+        playRound(playerSelection,computerSelection);
     }
 }
-const computerSelection = computerPlay();
-console.log ("Computer selection: " + computerSelection);  //Remove
-const playerSelection = "Scissors";
 
-playRound(playerSelection,computerSelection);
+game();
