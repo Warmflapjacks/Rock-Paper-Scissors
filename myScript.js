d27f8c72fe6bac88fa140aac119dcf76a9fcefdb
MoistButteryBiscuits <Mikeleblanc8489@gmail.com>
2021-12-04 12:41:34 -0500

    Add return and console.log() for testing

--- myScript.js
+++ myScript.js
@@ -11,42 +11,50 @@ function computerPlay () {
 }
 
 function playRound (playerSelection, computerSelection) {
+    //sends playerselection to lowercase
     let playerPick = playerSelection.toLowerCase();
 
     //if selections are a tie
     if (playerPick === computerSelection) {
-        console.log("Its a tie!")
+        console.log ("Its a tie!");
+        // return ("Its a tie!");
     }
     //used when not a tie
     else{
         if (playerPick === "paper") {
             if (computerSelection === "rock") {
-                console.log("You win! " + playerSelection + " beats " + computerSelection);
+                console.log ("You win! " + playerSelection + " beats " + computerSelection);
+                // return ("You win! " + playerSelection + " beats " + computerSelection);
             }
             else {
                 console.log("You lose! Scissors beats " + playerSelection);
+                // return ("You lose! Scissors beats " + playerSelection);
             }
         }
         if (playerPick === "rock") {
             if (computerSelection === "scissors") {
                 console.log("You win! " + playerSelection + " beats " + computerSelection);
+                // return ("You win! " + playerSelection + " beats " + computerSelection);
             }
             else {
                 console.log("You lose! Paper beats " + playerSelection);
+                // return ("You lose! Paper beats " + playerSelection);
             }
         }
         if (playerPick === "scissors") {
             if (computerSelection === "paper") {
                 console.log("You win! " + playerSelection + " beats " + computerSelection);
+                // return ("You win! " + playerSelection + " beats " + computerSelection);
             }
             else {
                 console.log("You lose! Rock beats " + playerSelection);
+                // return ("You lose! Rock beats " + playerSelection);
             }
         }
     }
 }
 const computerSelection = computerPlay();
-console.log("Computer selection: " + computerSelection);  //Remove
+console.log ("Computer selection: " + computerSelection);  //Remove
 const playerSelection = "Scissors";
 
 playRound(playerSelection,computerSelection);
