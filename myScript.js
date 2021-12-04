af3c1fc3ecf30d1c0428446bb2377ffc4fbf0bf9
MoistButteryBiscuits <Mikeleblanc8489@gmail.com>
2021-12-04 12:34:04 -0500

    Add additional logic for playerSelections

--- myScript.js
+++ myScript.js
@@ -1,27 +1,16 @@
-// function computerPlay () {
-//     let computerOption = [
-//         "rock",
-//         "paper",
-//         "scissors"
-//     ];
+function computerPlay () {
+    let computerOption = [
+        "rock",
+        "paper",
+        "scissors"
+    ];
 
-//     //randomly selects a computer pick from computerOption array
-//     let computerSelection = computerOption[Math.floor(Math.random() * computerOption.length)]
-//     return computerSelection
-// }
+    //randomly selects a computer pick from computerOption array
+    let computerSelection = computerOption[Math.floor(Math.random() * computerOption.length)]
+    return computerSelection
+}
 
 function playRound (playerSelection, computerSelection) {
-    /*Pseudo
-    -Paper beats rock
-    -Rock beats scissors
-    -Scissor beats paper
-    
-    -send playerSelection into a variable that converts string into lowercase
-    -compare between playerSelection and computerSelection
-    -declare a winner using playerSelection and computerSelection variables (from playerSelection perspective)
-    -RETURN ALL VALUES NOT CONSOLE.LOG()
-    */
-
     let playerPick = playerSelection.toLowerCase();
 
     //if selections are a tie
@@ -38,17 +27,26 @@ function playRound (playerSelection, computerSelection) {
                 console.log("You lose! Scissors beats " + playerSelection);
             }
         }
+        if (playerPick === "rock") {
+            if (computerSelection === "scissors") {
+                console.log("You win! " + playerSelection + " beats " + computerSelection);
+            }
+            else {
+                console.log("You lose! Paper beats " + playerSelection);
+            }
+        }
+        if (playerPick === "scissors") {
+            if (computerSelection === "paper") {
+                console.log("You win! " + playerSelection + " beats " + computerSelection);
+            }
+            else {
+                console.log("You lose! Rock beats " + playerSelection);
+            }
+        }
     }
 }
+const computerSelection = computerPlay();
+console.log("Computer selection: " + computerSelection);  //Remove
+const playerSelection = "Scissors";
 
-// REMOVE
-// tests output
-// console.log(computerPlay());
-
-// uncomment this line and line 40 when live
-// const computerSelection = computerPlay();
-const computerSelection = "scissors";
-const playerSelection = "Paper";
-
-// test output
-console.log(playRound(playerSelection,computerSelection));
+playRound(playerSelection,computerSelection);
