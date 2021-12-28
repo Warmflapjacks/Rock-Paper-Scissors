729909718514d45efc93836b1272b8052043e08d
MoistButteryBiscuits <Mikeleblanc8489@gmail.com>
2021-12-28 15:17:21 -0500

    Add running score and winner after 5 points

--- myScript.js
+++ myScript.js
@@ -1,4 +1,8 @@
-//computer selector
+//global variables
+let playerScore = 0;
+let computerScore = 0;
+
+//gets computer selection
 function getComputerSelection () {
     let computerOption = [
         "rock",
@@ -18,50 +22,63 @@ function playGame() {
     buttons.forEach((button) => {
         button.addEventListener('click', () => {
             let playerSelection = button.id;
-            // document.getElementById("result-container").innerHTML = (playerSelection);
 
             //gets computer selection
-            let computerSelection = getComputerSelection();
+            let computerSelection = getComputerSelection();  
 
             //call playRound function
-            playRound(playerSelection,computerSelection);
+            if ((playerScore === 5) || (computerScore === 5)) {
+                document.getElementById("final-result").textContent = "GAME OVER";
+            }
+            else {
+                playRound(playerSelection,computerSelection);
+            }
         })
     })
 }
 
 //define playRound. playerSelection and computerSelection are local variables in the function and are not global
 function playRound (playerSelection, computerSelection) {
-    
     //if selections are a tie
     if (playerSelection === computerSelection) {                 
-        document.getElementById("result-container").textContent = "Its a tie!";
+        document.getElementById("game-log").textContent = "Its a tie!";
     }
     //used when not a tie
-    else{
+    else {
         if (playerSelection === "paper") {                       
             if (computerSelection === "rock") {
-                document.getElementById("result-container").textContent = "You win! Paper beats Rock";  
+                document.getElementById("game-log").textContent = "You win! Paper beats Rock";
+                playerScore++;
             }
             else {
-                document.getElementById("result-container").textContent = "You lose! Scissors beats Paper";   
+                document.getElementById("game-log").textContent = "You lose! Scissors beats Paper";
+                computerScore++;
             }
         }
         if (playerSelection === "rock") {                        
             if (computerSelection === "scissors") {
-                document.getElementById("result-container").textContent = "You win! Rocks beats Scissors";   
+                document.getElementById("game-log").textContent = "You win! Rocks beats Scissors";
+                playerScore++;
             }
             else {
-                document.getElementById("result-container").textContent = "You lose! Paper beats Rock";   
+                document.getElementById("game-log").textContent = "You lose! Paper beats Rock";
+                computerScore++;
             }
         }
         if (playerSelection === "scissors") {                 
             if (computerSelection === "paper") {
-                document.getElementById("result-container").textContent = "You win! Scissors beats Paper";
+                document.getElementById("game-log").textContent = "You win! Scissors beats Paper";
+                playerScore++;
             }
             else {
-                document.getElementById("result-container").textContent = "You lose! Rock beats Scissors";
+                document.getElementById("game-log").textContent = "You lose! Rock beats Scissors";
+                computerScore++;
             }
         }
     }
+    //shows running score
+    document.getElementById("player-running-count").innerHTML = `${playerScore}`;
+    document.getElementById("computer-running-count").innerHTML = `${computerScore}`;
 }
+
 playGame();
