5659c2393694654e74aba8c0bf8aaeab00cd7467
MoistButteryBiscuits <Mikeleblanc8489@gmail.com>
2021-12-30 15:20:32 -0500

    Change names of rock, paper, scissors

--- myScript.js
+++ myScript.js
@@ -47,31 +47,31 @@ function playRound (playerSelection, computerSelection) {
     else {
         if (playerSelection === "paper") {                       
             if (computerSelection === "rock") {
-                document.getElementById("game-log").textContent = "You win! Paper beats Rock";
+                document.getElementById("game-log").textContent = "You win! Walker Arrows beats Dragons";
                 playerScore++;
             }
             else {
-                document.getElementById("game-log").textContent = "You lose! Scissors beats Paper";
+                document.getElementById("game-log").textContent = "You lose! Dragonglass beats Walker Arrows";
                 computerScore++;
             }
         }
         if (playerSelection === "rock") {                        
             if (computerSelection === "scissors") {
-                document.getElementById("game-log").textContent = "You win! Rocks beats Scissors";
+                document.getElementById("game-log").textContent = "You win! Dragons beats Dragonglass";
                 playerScore++;
             }
             else {
-                document.getElementById("game-log").textContent = "You lose! Paper beats Rock";
+                document.getElementById("game-log").textContent = "You lose! Walker Arrows beats Dragons";
                 computerScore++;
             }
         }
         if (playerSelection === "scissors") {                 
             if (computerSelection === "paper") {
-                document.getElementById("game-log").textContent = "You win! Scissors beats Paper";
+                document.getElementById("game-log").textContent = "You win! Dragonglass beats Walker Arrows";
                 playerScore++;
             }
             else {
-                document.getElementById("game-log").textContent = "You lose! Rock beats Scissors";
+                document.getElementById("game-log").textContent = "You lose! Dragons beats Dragonglass";
                 computerScore++;
             }
         }
